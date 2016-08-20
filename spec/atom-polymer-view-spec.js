aee3f53b1f17b5d4ebc82df1ff9ecfb56b67722d
Peter Burns <rictic@google.com>
2016-08-20 08:39:44 -0700

    Initial commit

---
+++ spec/atom-polymer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomPolymerView from '../lib/atom-polymer-view';
+
+describe('AtomPolymerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
