25bdd2c50f495f85ffb88d4f5140ca8cbfe8e4db
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2016-11-21 17:44:57 +0100

    Split out classes to separate files (#44)
    * Split out classes to separate files
    
    * Update dependency versions and add license headers

---
+++ src/auto-completer.ts
@@ -0,0 +1,102 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+import * as autocomplete from 'atom-autocomplete-plus';
+import {RemoteEditorService} from 'polymer-editor-service/lib/remote-editor-service';
+
+class Autocompleter implements autocomplete.Provider {
+  selector = '.text.html, .source.js';
+  priority = 1;
+  editorService: RemoteEditorService;
+
+  async getSuggestions(options: autocomplete.SuggestionRequestOptions):
+      Promise<autocomplete.Suggestion[]> {
+    try {
+      return await this._getSuggestions(options);
+    } catch (e) {
+      // Atom pops up huge loud error popups so better to just log and swallow.
+      console.error(e.stack || e.message || e);
+      return [];
+    }
+  }
+
+  private async _getSuggestions(options: autocomplete.SuggestionRequestOptions):
+      Promise<autocomplete.Suggestion[]> {
+    if (!this.editorService) {
+      return [];
+    }
+
+    const position = {
+      line: options.bufferPosition.row,
+      column: options.bufferPosition.column
+    };
+    const relativePath: string =
+        atom.project.relativizePath(options.editor.getPath())[1];
+    const completions =
+        await this.editorService.getTypeaheadCompletionsAtPosition(
+            relativePath, position);
+    console.log(completions);
+    if (!completions) {
+      return [];
+    }
+    if (completions.kind === 'element-tags') {
+      // Could do something more clever here, and look for partial matches
+      // ordering by length of match.
+      const matchingElements = completions.elements.filter(
+          e => e.tagname.startsWith(options.prefix));
+      return matchingElements.map((element) => {
+        const suggestion: autocomplete.SnippetSuggestion = {
+          snippet: element.expandToSnippet,
+          displayText: `<${element.tagname}>`,
+          description: element.description,
+          descriptionMarkdown: element.description,
+          type: 'class',
+          replacementPrefix: `<${options.prefix}`
+        };
+        return suggestion;
+      });
+    } else if (completions.kind === 'attributes') {
+      return completions.attributes.map((attr) => {
+        let suggestion: autocomplete.Suggestion;
+        if (attr.type === 'boolean') {
+          suggestion = {text: attr.name};
+        } else {
+          suggestion = {
+            displayText: attr.name,
+            snippet: `${attr.name}="\${1:${attr.type}}"`
+          };
+        }
+        suggestion.type = 'property';
+        suggestion.description = attr.description;
+        if (attr.inheritedFrom) {
+          suggestion.rightLabel = `⊃ ${attr.inheritedFrom}`;
+        }
+        if (attr.type) {
+          suggestion.leftLabel = attr.type;
+        }
+        return suggestion;
+      });
+    } /* else if (completions.kind === 'resource-paths') {
+      return completions.paths.map((path) => {
+        const suggestion: autocomplete.TextSuggestion = {
+          text: path,
+          replacementPrefix: completions.prefix,
+          type: 'import'
+        };
+        return suggestion;
+      });
+    } */
+  }
+}
+
+export default Autocompleter;
--- src/polymer-ide.ts
+++ src/polymer-ide.ts
@@ -1,15 +1,26 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
 'use babel';
 
 /// <reference path="../custom_typings/main.d.ts" />
 
-import * as path from 'path';
-
 import {CompositeDisposable} from 'atom';
 import * as lint from 'atom-lint';
-import {Severity} from 'polymer-analyzer/lib/warning/warning';
 import {RemoteEditorService} from 'polymer-editor-service/lib/remote-editor-service';
-import {SourceRange} from 'polymer-analyzer/lib/model/model';
 import * as autocomplete from 'atom-autocomplete-plus';
+import Autocompleter from './auto-completer';
+import Linter from './linter';
 import TooltipManager from './tooltip-manager';
 
 interface ViewState {}
@@ -77,165 +88,5 @@ class PolymerIde {
     return this.autocompleter;
   };
 };
-
-class Linter implements lint.Provider {
-  name = 'polymer-ide';
-  grammarScopes = ['source.js', 'text.html', 'text.html.basic'];
-  scope: 'file' = 'file';
-  lintOnFly = true;
-  configurationError: string|null = null;
-  editorService: RemoteEditorService;
-
-  async lint(textEditor: AtomCore.IEditor): Promise<lint.Message[]> {
-    try {
-      return await this._lint(textEditor);
-    } catch (e) {
-      // Atom pops up huge loud error popups if we return a rejecting promise
-      // here, so better to just log and swallow.
-      console.error(e.stack || e.message || e);
-      return [];
-    }
-  };
-
-  private async _lint(textEditor: AtomCore.IEditor): Promise<lint.Message[]> {
-    if (this.configurationError) {
-      return [{
-        type: 'Error',
-        text: this.configurationError,
-        range: [[0, 0], [0, 1]],
-        filePath: textEditor.getPath(),
-        severity: 'error'
-      }];
-    }
-    const [projectPath, relativePath]: string[] =
-        atom.project.relativizePath(textEditor.getPath());
-    try {
-      await this.editorService.fileChanged(
-          relativePath, textEditor.getBuffer().cachedText);
-    } catch (e) {
-      /* swallow the erorr, let getWarningsFor() handle things */
-    }
-    const warnings = await this.editorService.getWarningsForFile(relativePath);
-    return warnings.map(w => {
-      const {path: relPath, range} = convertSourceRange(w.sourceRange);
-      return <lint.Message>{
-        type: severityToMessageType(w.severity),
-        filePath: path.join(projectPath, relPath),
-        range: range,
-        text: w.message
-      };
-    });
-  }
-}
-
-class Autocompleter implements autocomplete.Provider {
-  selector = '.text.html, .source.js';
-  priority = 1;
-  editorService: RemoteEditorService;
-
-  async getSuggestions(options: autocomplete.SuggestionRequestOptions):
-      Promise<autocomplete.Suggestion[]> {
-    try {
-      return await this._getSuggestions(options);
-    } catch (e) {
-      // Atom pops up huge loud error popups so better to just log and swallow.
-      console.error(e.stack || e.message || e);
-      return [];
-    }
-  }
-
-  private async _getSuggestions(options: autocomplete.SuggestionRequestOptions):
-      Promise<autocomplete.Suggestion[]> {
-    if (!this.editorService) {
-      return [];
-    }
-
-    const position = {
-      line: options.bufferPosition.row,
-      column: options.bufferPosition.column
-    };
-    const relativePath: string =
-        atom.project.relativizePath(options.editor.getPath())[1];
-    const completions =
-        await this.editorService.getTypeaheadCompletionsAtPosition(
-            relativePath, position);
-    console.log(completions);
-    if (!completions) {
-      return [];
-    }
-    if (completions.kind === 'element-tags') {
-      // Could do something more clever here, and look for partial matches
-      // ordering by length of match.
-      const matchingElements = completions.elements.filter(
-          e => e.tagname.startsWith(options.prefix));
-      return matchingElements.map((element) => {
-        const suggestion: autocomplete.SnippetSuggestion = {
-          snippet: element.expandToSnippet,
-          displayText: `<${element.tagname}>`,
-          description: element.description,
-          descriptionMarkdown: element.description,
-          type: 'class',
-          replacementPrefix: `<${options.prefix}`
-        };
-        return suggestion;
-      });
-    } else if (completions.kind === 'attributes') {
-      return completions.attributes.map((attr) => {
-        let suggestion: autocomplete.Suggestion;
-        if (attr.type === 'boolean') {
-          suggestion = {text: attr.name};
-        } else {
-          suggestion = {
-            displayText: attr.name,
-            snippet: `${attr.name}="\${1:${attr.type}}"`
-          };
-        }
-        suggestion.type = 'property';
-        suggestion.description = attr.description;
-        if (attr.inheritedFrom) {
-          suggestion.rightLabel = `⊃ ${attr.inheritedFrom}`;
-        }
-        if (attr.type) {
-          suggestion.leftLabel = attr.type;
-        }
-        return suggestion;
-      });
-    } /* else if (completions.kind === 'resource-paths') {
-      return completions.paths.map((path) => {
-        const suggestion: autocomplete.TextSuggestion = {
-          text: path,
-          replacementPrefix: completions.prefix,
-          type: 'import'
-        };
-        return suggestion;
-      });
-    } */
-  }
-}
-
-function severityToMessageType(severity: Severity): string {
-  switch (severity) {
-    case Severity.ERROR:
-      return 'Error';
-    case Severity.WARNING:
-      return 'Warning';
-    case Severity.INFO:
-      return 'Info';
-    default:
-      const never: never = severity;
-      throw new Error(`Unknown severity received: ${never}`);
-  }
-}
-
-function convertSourceRange(sourceRange: SourceRange):
-    {range: lint.Range, path: string} {
-  return {
-    path: sourceRange.file,
-    range: [
-      [sourceRange.start.line, sourceRange.start.column],
-      [sourceRange.end.line, sourceRange.end.column]
-    ]
-  };
-}
 
 export default new PolymerIde();
