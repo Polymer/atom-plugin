9d7bbae4f234261874d3bb055110d897825c876c
43081j <43081j@users.noreply.github.com>
2017-03-15 23:17:52 +0000

    add spec fixture

--- spec/linter-spec.js
+++ spec/linter-spec.js
@@ -1,23 +1,16 @@
 'use babel';
 
+import * as path from 'path';
 import PolymerIde from '../lib/polymer-ide';
-import temp from 'temp';
 import { Severity } from 'polymer-analyzer/lib/warning/warning';
-
-temp.track();
 
 describe('Linter', () => {
   let provider;
   let editor;
-  let filePath;
-  let projectPath = temp.mkdirSync('dir1');
+  let projectPath = path.resolve(__dirname, 'fixtures');
+  let filePath = path.resolve(projectPath, 'simple.html');
 
   beforeEach(() => {
-    filePath = temp.openSync({
-      suffix: '.html',
-      dir: projectPath
-    }).path;
-
     atom.project.setPaths([projectPath]);
 
     waitsForPromise(() =>
@@ -35,17 +28,18 @@ describe('Linter', () => {
     it('should return a configuration error if any set', () => {
       provider.configurationError = 'i am an error';
 
-      return provider.lint(editor).then(warnings => {
-        expect(warnings.length).toBe(1);
+      waitsForPromise(() =>
+        provider.lint(editor).then(warnings => {
+          expect(warnings.length).toBe(1);
 
-        expect(warnings[0]).toEqual({
-          type: 'Error',
-          text: 'i am an error',
-          range: [[0, 0], [0, 1]],
-          filePath: filePath,
-          severity: 'error'
-        });
-      });
+          expect(warnings[0]).toEqual({
+            type: 'Error',
+            text: 'i am an error',
+            range: [[0, 0], [0, 1]],
+            filePath: filePath,
+            severity: 'error'
+          });
+        }));
     });
 
     it('should log an exception if any uncaught', () => {
@@ -54,40 +48,24 @@ describe('Linter', () => {
           message: 'foo'
         });
 
-      return provider.lint(editor).then(warnings => {
-        expect(warnings.length).toBe(0);
-        expect(console.error).toHaveBeenCalledWith('foo');
-      });
+      waitsForPromise(() =>
+        provider.lint(editor).then(warnings => {
+          expect(warnings.length).toBe(0);
+          expect(console.error).toHaveBeenCalledWith('foo');
+        }));
     });
 
     it('should return warnings if any', () => {
-      spyOn(provider.editorService, 'getWarningsForFile')
-        .andReturn(Promise.resolve([{
-          message: 'foo error',
-          severity: Severity.ERROR,
-          code: '',
-          sourceRange: {
-            file: filePath,
-            start: {
-              line: 0,
-              column: 0
-            },
-            end: {
-              line: 1,
-              column: 1
-            }
-          }
-        }]));
-
-      return provider.lint(editor).then(warnings => {
-        expect(warnings.length).toBe(1);
-        expect(warnings[0]).toEqual({
-          type: 'Error',
-          filePath: filePath,
-          range: [[0, 0], [1, 1]],
-          text: 'foo error'
-        });
-      });
+      waitsForPromise(() =>
+        provider.lint(editor).then(warnings => {
+          expect(warnings.length).toBe(1);
+          expect(warnings[0]).toEqual({
+            type: 'Error',
+            filePath: filePath,
+            range: [[0, 0], [1, 1]],
+            text: 'foo error'
+          });
+        }));
     });
   });
 });
